c0ea24dd00e976afc0ff6f8c11699e78e0ef1ebf
K R Anand <anandkryelwal@gmail.com>
2018-02-15 21:19:57 +0530

    Update FrontEnd.py

--- FrontEnd.py
+++ FrontEnd.py
@@ -1,9 +1,12 @@
+# This is the front end
+
+# Import all the modules 
 from AddressBookDB import *
 from csv import *
 
-
+# The main menu 
 print("#"*80)
-print("ADDRESS BOOK MENU".title())
+print("ADDRESS BOOK MENU".center(80))
 print("#"*80)
 print("\n")
 print("What would you like to do?")
@@ -16,6 +19,7 @@ print("Press e to export all records to .csv file")
 print("\n")
 choice = input("Press the key : ")
 
+# Input the value and call AddressBook.py fuction writeIntoDB()
 if choice == "i":
 	print("\n")
 	try:
@@ -26,18 +30,21 @@ if choice == "i":
             print ("The values are not acceptable")
 	writeIntoDB(name,ph_no,email)
 
+# Call AddressBook.py function readIntoDB() 
 elif choice == "v":
 	print("\n")
 	readIntoDB()
 
+# Search AddressBook.py function searchName()
+# Split names by spaces
 elif choice == "s":
 	print("\n")
-	sname = str(input("Enter the names : "))
+	sname = str(input("Enter the names delimited by spaces : "))
 	sname.strip()
 	slst = sname.split(" ")
 	for name in slst:
 	    searchName(name)
-
+#Calls clearRecords() deletes all records
 elif choice == "c":
 	print("\n")
 	clearRecords()
@@ -53,3 +60,6 @@ elif choice == "d":
 elif choice == "e":
         print("\n")
         writeCSV()
+	
+else:
+	print("Improper Input")
